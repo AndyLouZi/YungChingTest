e8d2cfe612725070a9ce661e4cb4b4ac11f263c7
AndyLouZi <loubao996584@gmail.com>
2024-12-18 21:22:55 +0800

    加入專案檔案。

---
+++ YungChingTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YungChingTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
